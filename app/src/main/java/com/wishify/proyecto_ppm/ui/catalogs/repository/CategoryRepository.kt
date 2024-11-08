922ac72f49e160ae5277d03f99c2d5a2ecb6b46d
Iris <irisale.ayala@gmail.com>
2024-11-07 21:47:04 -0600

    rep y viewmodel

--- app/src/main/java/com/wishify/proyecto_ppm/ui/catalogs/repository/CategoryRepository.kt
+++ app/src/main/java/com/wishify/proyecto_ppm/ui/catalogs/repository/CategoryRepository.kt
@@ -2,14 +2,14 @@ package com.wishify.proyecto_ppm.ui.catalogs.repository
 
 import com.wishify.proyecto_ppm.networking.WishWebService
 import com.wishify.proyecto_ppm.networking.response.WishCategory
-import com.wishify.proyecto_ppm.networking.response.WishFilter
+import com.wishify.proyecto_ppm.networking.response.WishProduct
 
 class CategoryRepository(private val webService: WishWebService = WishWebService()) {
 
     suspend fun getWishCategories(): List<WishCategory>{
         return webService.getWishCategories().categories
     }
-    suspend fun filterByCategory(categoryID: Int): List<WishFilter> {
+    suspend fun filterByCategory(categoryID: Int): List<WishProduct> {
         return webService.getCategoryFilter(categoryID).products
     }
 }
