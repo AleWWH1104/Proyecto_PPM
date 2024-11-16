a108f1ddf59296e509449bcaa82d00d98791b839
David <dominguezdavid396@gmail.com>
2024-11-16 11:39:19 -0600

    Implementacion de SignIn funcional

--- app/src/main/java/com/wishify/proyecto_ppm/ui/account/view/SignIn_Screen.kt
+++ app/src/main/java/com/wishify/proyecto_ppm/ui/account/view/SignIn_Screen.kt
@@ -1,5 +1,6 @@
 package com.wishify.proyecto_ppm.ui.account.view
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -7,12 +8,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -31,9 +34,42 @@ import com.wishify.proyecto_ppm.ui.elements.topNavBar
 
 @Composable
 fun SignInScreen(navController: NavController) {
-    // Estados para Username y Password
-    val username = remember { mutableStateOf("") }
+    val auth = FirebaseAuth.getInstance()
+    val email = remember { mutableStateOf("") }
     val password = remember { mutableStateOf("") }
+    val context = LocalContext.current // Obtén el contexto para usar con Toast
+    println("al iniciar Email: ${email.value}, Password: ${password.value}")
+
+
+    fun tryLogIn() {
+        println("tryLogIn called")
+        println(" en trylogin Email: ${email.value}, Password: ${password.value}")
+
+        try {
+            auth.signInWithEmailAndPassword(email.value, password.value)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        println("Login successful")
+                        navController.navigate(NavigationState.AllLists.route)
+                    } else {
+                        println("Sign-in failed: ${task.exception?.localizedMessage}")
+                        Toast.makeText(
+                            context,
+                            "Sign-in failed: ${task.exception?.localizedMessage ?: "Unknown error"}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        } catch (e: Exception) {
+            println("Exception during login: ${e.localizedMessage}")
+            Toast.makeText(
+                context,
+                "An error occurred: ${e.localizedMessage ?: "Unknown error"}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 
     Scaffold(
         topBar = { topNavBar(navController = navController) }
@@ -70,13 +106,13 @@ fun SignInScreen(navController: NavController) {
                 verticalArrangement = Arrangement.Center
             ) {
                 Text(
-                    text = "Username",
+                    text = "Email",
                     style = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp),
                     color = Color.White
                 )
                 smallTexFieldSignIn(
-                    text = username.value,
-                    onTextChange = { username.value = it } // Actualiza el estado
+                    text = email.value,
+                    onTextChange = { email.value = it } // Actualiza el estado
                 )
                 Spacer(modifier = Modifier.padding(16.dp))
                 Text(
@@ -91,9 +127,16 @@ fun SignInScreen(navController: NavController) {
                 Spacer(modifier = Modifier.padding(16.dp))
                 LargeButtons(
                     texto = R.string.signIn,
-                    onClick = { navController.navigate(NavigationState.AllLists.route) },
+
+                    onClick = {
+                        println(" en el boton Email: ${email.value}, Password: ${password.value}")
+
+                        tryLogIn()
+                        Toast.makeText(context, "Button works!", Toast.LENGTH_SHORT).show()
+                              },
                     buttonColor = Color(0xFFfef0e1),
-                    textColor = Color(0xFFb2422d)
+                    textColor = Color(0xFFb2422d),
+                    enabled = email.value.isNotEmpty() && password.value.isNotEmpty()
                 )
             }
             Image(
